4314d4791f9a81bea2298dd8999215b2217ec3b1
singhpriyanhsu9415 <148875469+singhpriyanhsu9415@users.noreply.github.com>
2025-04-29 16:51:42 +0530

    vite and vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist'  // This is the default, but confirm it's set correctly
+  }
 })
