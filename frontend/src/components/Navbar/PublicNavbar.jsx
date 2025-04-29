d3881aabd0263b2f6555482f986eeca722327a5b
singhpriyanhsu9415 <148875469+singhpriyanhsu9415@users.noreply.github.com>
2025-04-29 16:32:14 +0530

    removing img

--- frontend/src/components/Navbar/PublicNavbar.jsx
+++ frontend/src/components/Navbar/PublicNavbar.jsx
@@ -19,7 +19,7 @@ export default function PublicNavbar() {
                 <div className="-ml-2 mr-2 flex items-center md:hidden">
                   {/* Mobile menu button */}
                   <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <img src="/tackwallet.png">imgage</img>
+                    
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Open main menu</span>
                     {open ? (
